283459cfb6e1703f2642f15fac13ac98d01a2580
majo0518 <majovargas0518@gmail.com>
2023-08-10 08:39:21 -0500

    Agregar archivos de proyecto.

---
+++ ChatLinea.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace chat2Prueba
+{
+    public class ChatLinea : Hub
+    {
+        public void chat(string nombre, string mensaje)
+        {
+            Clients.All.chatmaria(nombre, mensaje); // queremos que el cliente nos envie a todos la funcion que yo quiera y le ponemos el nombre que queramos en este caso chatmaria este recibe el nombre y el mensaje y se lo envia a todos los que estan en linea 
+        }
+    }
+}
---
+++ Startup1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(chat2Prueba.Startup1))]
+
+namespace chat2Prueba
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR(); // este inicializa en caso de que tenga varios hubs  pero como solo hay uno inicializa uno 
+        }
+    }
+}
